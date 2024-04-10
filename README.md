0f2505b5a3300cfeec76c56369cbd51785e9141f
datehubin <didehtmd@gmail.com>
2024-04-10 18:44:58 +0900

    초기 환경 세팅 및 로딩/에러 조건 값 생성

---
+++ README.md
@@ -0,0 +1,5 @@
+# 초기 세팅 #
+-- react / typescript
+-- style / scss
+-- yarn / craco
+-- eslint / prettier
---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
+import styles from './app.module.scss'
+import FullScreenMessage from '@components/shared/FullScreenMessage'
+const cx = classNames.bind(styles)
+
+function App() {
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        }
+
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+      })
+      .catch((e) => {
+        console.log('에러발생', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+  if (loading === false) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
+}
+
+export default App
